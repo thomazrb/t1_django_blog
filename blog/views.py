6483ff1a5ff07f23edbf828f10af01d59ac9e659
Thomaz Botelho <thomazrb@gmail.com>
2024-05-11 17:29:50 -0300

    Usuários

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 from django.urls import reverse_lazy
 
@@ -11,17 +12,17 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_create.html'
     fields = ['title', 'author', 'content']
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = 'post_update.html'
     fields = ['title', 'content']
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('usuarios/', include('django.contrib.auth.urls')),
     path('', include('blog.urls'))
 ]
