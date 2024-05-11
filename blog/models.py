addb47dd69bd4817de1ef60e3b6aa74d74af0cb8
Thomaz Botelho <thomazrb@gmail.com>
2024-05-11 15:14:46 -0300

    Posts associados à um usuário

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=150)
-    author = models.CharField(max_length=50)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
     content = models.TextField()
 
     def __str__(self):
