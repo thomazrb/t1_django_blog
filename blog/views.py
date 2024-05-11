256c6737f7b99aab2f5748a8cec7f1af69b3ed78
Thomaz Botelho <thomazrb@gmail.com>
2024-05-11 17:11:21 -0300

    Delete do crud

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
 class BlogListView(ListView):
@@ -19,3 +20,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_update.html'
     fields = ['title', 'content']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
